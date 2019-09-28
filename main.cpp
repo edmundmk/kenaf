b280759b41d811964810569711b00f68bb6dda09
Edmund Kapusniak <edmund.kapusniak@hotmail.com>
2019-09-28 15:33:47 +0100

    add framework to actually build some stuff.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  kenaf
+//
+//  Created by Edmund Kapusniak on 28/09/2019.
+//  Copyright © 2019 Edmund Kapusniak.
+//
+//  Licensed under the MIT License. See LICENSE file in the project root for
+//  full license information.
+//
+
+#include <stdlib.h>
+
+int main( int argc, char* argv[] )
+{
+    return EXIT_SUCCESS;
+}
+
