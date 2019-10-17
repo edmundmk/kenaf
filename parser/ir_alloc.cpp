b8a83d582ddc5acec12670faa55c35cf7f6478db
Edmund Kapusniak <edmund.kapusniak@hotmail.com>
2019-10-17 09:06:06 +0100

    register allocation approach described.  there must be a simpler way, but this is what we're going for...

--- parser/ir_alloc.cpp
+++ parser/ir_alloc.cpp
@@ -13,6 +13,57 @@
 namespace kf
 {
 
+/*
+    The register allocation algorithm is described here.
+
+    The live ranges of normal locals and of for loop implicit variables are
+    constructed.  The live range of a normal variable is a list of ops that
+    define the variable, plus PHI/REF ops that import it into a block, in
+    order.  The live range of an implicit for loop variable is the entire
+    range of the loop.
+
+    A variable is identified with the first op that declares it.  For loop
+    variables are identified with the JUMP_FOR_EACH or JUMP_FOR_STEP ops.
+
+    Each value (instruction result, local, implicit for) has its live range
+    examined.  Values which die at pinning instructions are *pinned*.  Register
+    allocation for pinned instructions is delayed until the pinning instruction
+    is allocated.
+
+      - A pinning instruction is one of JUMP_RETURN, CALL, YCALL, YIELD,
+        MOV, or B_PHI.  To pin a value, these instructions must use that value
+        as an operand.
+
+    Each stack top instruction is associated with the set of values which is
+    live across it.
+
+      - A stack top instruction is one of CALL, YCALL or YIELD.
+
+      - Instructions VARARG, UNPACK, EXTEND, JUMP_FOR_EACH, JUMP_FOR_STEP, and
+        FOR_EACH_ITEMS are also allocated at the stack top but they do not pin
+        values, and so there is no advantage in allocating them early.
+
+      - A stack top instruction can itself be pinned.
+
+    A stack top instruction is register-allocated as soon as all values which
+    are live across it are allocated.
+
+    Then we do a modified linear scan, looking ahead to stack-top instructions
+    once all stacked values have been allocated, and backtracking once values
+    become unpinned.
+
+    A value is preferentially allocated to the following registers:
+
+      - If it's a parameter, register 1 + parameter index.
+
+      - The register its pinning instruction needs it to be in.
+
+      - The lowest numbered free register.
+
+    Ranges where registers are allocated are tracked in a data structure which
+    stores allocated live ranges for each register.
+*/
+
 ir_alloc::ir_alloc( source* source )
     :   _source( source )
 {
--- parser/ir_alloc.h
+++ parser/ir_alloc.h
@@ -18,6 +18,9 @@
 */
 
 #include "ir.h"
+#include <vector>
+#include <unordered_map>
+#include <queue>
 
 namespace kf
 {
@@ -33,8 +36,32 @@ public:
 
 private:
 
+    struct live_index
+    {
+        unsigned value;         // index of declaring instruction.
+        unsigned local;         // local number (IR_INVALID_LOCAL for for)
+        unsigned live_index;    // index in value_live_ranges.
+        unsigned live_count;    // count of entries in value_live_ranges.
+    };
+
+    struct live_range
+    {
+        unsigned lower;         // instruction value becomes live/block start
+        unsigned upper;         // instruction value dies/block end.
+    };
+
     source* _source;
     ir_function* _f;
+
+    std::vector< unsigned > _local_value_index;     // local index -> value_live_index entry
+    std::vector< live_index > _value_live_index;    // instruction index -> value_live_ranges range
+    std::vector< live_range > _value_live_ranges;   // individual live range.
+
+    // instruction index -> call instruction index[]
+    std::unordered_multimap< unsigned, unsigned > _live_across;
+
+    // unpinned values in order of instruction index.
+    std::priority_queue< unsigned, std::vector< unsigned >, std::greater< unsigned > > _unpinned;
 
 };
 
