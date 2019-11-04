e156a3f6887dddd49abf570aebbf916b95483b55
Edmund Kapusniak <edmund.kapusniak@hotmail.com>
2019-11-04 10:41:11 +0000

    and some initial implementation of it.

--- source/runtime/corlib/corprint.cpp
+++ source/runtime/corlib/corprint.cpp
@@ -9,5 +9,39 @@
 //
 
 #include "cormath.h"
+#include <stdio.h>
 
+namespace kf
+{
+
+/*
+    print has a printf-like format.
+*/
+
+static size_t print( void* cookie, frame* frame, const value* arguments, size_t argcount )
+{
+    std::string_view text = get_text( arguments[ 0 ] );
+    const char* s = text.data();
+    const char* e = text.data() + text.size();
+
+    size_t argindex = 1;
+    for ( const char* p = s; p < e; ++p )
+    {
+        if ( p[ 0 ] == '%' )
+        {
+            printf( "%.*s", (int)( p - s ), s );
+        }
+
+    }
+
+    return rvoid( frame );
+}
+
+void expose_corprint()
+{
+    value global = global_object();
+    set_key( global, "print", create_function( print, nullptr, 1, PARAM_VARARG ) );
+}
+
+}
 
