bf14e7863562a364bfcbfaf72fce82ddafbc4d81
Edmund Kapusniak <edmund.kapusniak@hotmail.com>
2019-11-04 10:33:30 +0000

    starting to work on print() function.

---
+++ source/runtime/corlib/corprint.cpp
@@ -0,0 +1,13 @@
+//
+//  cormath.cpp
+//
+//  Created by Edmund Kapusniak on 02/11/2019.
+//  Copyright © 2019 Edmund Kapusniak.
+//
+//  Licensed under the MIT License. See LICENSE file in the project root for
+//  full license information.
+//
+
+#include "cormath.h"
+
+
---
+++ source/runtime/corlib/corprint.h
@@ -0,0 +1,22 @@
+//
+//  corprint.h
+//
+//  Created by Edmund Kapusniak on 03/11/2019.
+//  Copyright © 2019 Edmund Kapusniak.
+//
+//  Licensed under the MIT License. See LICENSE file in the project root for
+//  full license information.
+//
+
+#ifndef KF_CORPRINT_H
+#define KF_CORPRINT_H
+
+namespace kf
+{
+
+void expose_corprint();
+
+}
+
+#endif
+
